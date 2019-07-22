e970b73a2fed3cde074c25d022a8ce42cfcd6945
casper_yue@163.com <casper_yue@163.com>
2019-07-22 16:11:42 +0800

    change front-end

--- back-end/app/models.py
+++ back-end/app/models.py
@@ -1,5 +1,8 @@
-from flask import url_for
+import base64
+from datetime import datetime, timedelta
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import url_for
 from app import db
 
 
@@ -32,9 +35,17 @@ class User(PaginatedAPIMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))  # 不保存原始密码
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self, include_email=False):
         data = {
@@ -55,9 +66,21 @@ class User(PaginatedAPIMixin, db.Model):
         if new_user and 'password' in data:
             self.set_password(data['password'])
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
 
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
