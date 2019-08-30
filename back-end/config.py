763f81e1ce77f09722af1429bc3155abe058e09f
casper_yue@163.com <casper_yue@163.com>
2019-08-30 14:23:12 +0800

    sa

--- back-end/config.py
+++ back-end/config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:casper@192.168.45.160:3306/madblog"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:casper@192.168.45.160:13306/madblog"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
     USERS_PER_PAGE = 10
